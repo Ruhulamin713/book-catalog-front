abd8c0fce1386907ce2461e973303310385f01e2
Ruhulamin713 <ruhulamin.milon7@gmail.com>
2023-08-18 15:54:49 +0600

    shadcn added .

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { RouterProvider } from "react-router-dom";
-import routes from "./routes/routes.tsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
